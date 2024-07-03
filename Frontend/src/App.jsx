fc5b0a468ade69fb2694a02564ca49d186edca53
shivraj <sk40fs@gmail.com>
2024-07-03 19:55:39 +0530

    new

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SearchNav, Navbar } from "./components/index";
+import { SearchNav, Navbar } from "./Components/index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
   Offers,
